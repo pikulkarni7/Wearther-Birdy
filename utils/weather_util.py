833181d2b079b638dfe6d64da73b68d96871b6e2
pikulkarni7 <pkulkarni@scu.edu>
2022-11-01 09:38:33 -0700

    filtered weather data

--- utils/weather_util.py
+++ utils/weather_util.py
@@ -1,3 +1,4 @@
+from traceback import print_tb
 import requests
 import json
 from rest_framework import status
@@ -18,7 +19,6 @@ def fetchweather_coords(request):
     
     if request.method=='GET':
         try:
-            print(request.query_params)
             
             coords = request.query_params
             
@@ -35,11 +35,51 @@ def fetchweather_coords(request):
             
             r = requests.get('https://api.openweathermap.org/data/2.5/weather', 
                             params=payload)
+            
+            data = json.loads(r.text)
+            
+            filtered_data = filter_weather_data(data)
 
-            return Response(r.json(), status=status.HTTP_200_OK)
+            return Response(filtered_data, status=status.HTTP_200_OK)
         
         except Exception as e:
             print(e)
             return Response({"message" : "Couldn't fetch weather"})
     
+    
+
+
+def filter_weather_data(weather) -> dict:
+    
+    fields = {
+        "name",
+        'dt',
+        'description',         #main weather description
+        'temp',
+        'temp_min',
+        'temp_max',
+        'speed',               #wind speed
+        '1h',                  #rain 1 hr
+        'country',
+        'pressure',
+    }
+    
+    result = {}
+    
+    for key, value in weather.items():
+        if key in fields:
+            result[key] = value
+            
+        if isinstance(value, dict):
+                result.update(filter_weather_data(value))      
+        
+        if isinstance(value, list):
+            for i in value:
+                if isinstance(i, dict):
+                    result.update(filter_weather_data(i))
+                    
+    if 'rain' not in weather.keys():
+        result['rain'] = 'N/A'
+                
+    return result
     
