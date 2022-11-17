576a86b104f7298856a53b2c8e5a3af40ce1b081
pikulkarni7 <pkulkarni@scu.edu>
2022-11-16 19:12:34 -0800

    session trial

--- utils/urls.py
+++ utils/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from .weather import fetchweather_coords
+from .session import post_session_variable, get_session_variable
 
 urlpatterns = [
     path('weather/fetchcurrent/', fetchweather_coords),
-
+    path('sessions/postvariable/', post_session_variable),
+    path('sessions/getvariable/', get_session_variable),
+    
 ]
